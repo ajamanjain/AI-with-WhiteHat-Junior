734f8170a126057233d7d952c6e05c7074ba9b6c
Aman Jain <ajjaina.jain@gmail.com>
2021-10-18 22:15:12 +0530

    Printing name and hobby
    Here you need to get the name and favourite hobby from the user and display it
    
    For example:
             Enter your name : Sam
             Enter your favourite hobby : cycling
             Hello Sam, your favourite hobby is cycling
    
    Follow the steps given below to achieve the desired result:
    Step 1: Ask the user to enter his/her name using the input() function and store it in the name variable.
    Step 2: Ask the user to enter his/her favourite hobby using the input() function and store it in the hobby variable.
    Step 3: Print the values of name and hobby variables using the print() function.
    
    Hint: print("Hello", name,", your favourite hobby is", hobby)

---
+++ 1: Printing name and hobby.py
@@ -0,0 +1,10 @@
+# Write your code here
+
+# Step 1: Get name from user and store it in variable 'name'
+name = input("Enter your name : ")
+
+# Step 2: Get hobby from user and store it in variable 'hobby'
+hobby = input("Enter your favourite hobby : ")
+
+# Step 3: Print name and hobby
+print("Hello", name, ",your favourite hobby is", hobby)
