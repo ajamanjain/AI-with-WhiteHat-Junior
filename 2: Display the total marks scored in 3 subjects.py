26a6c3cdfd7069b7254e10fc4ba514a929108027
Aman Jain <ajjaina.jain@gmail.com>
2021-10-18 22:16:55 +0530

    Display the total marks scored in 3 subjects
    Calculate the total marks scored in 3 subjects (100 marks each) and display the score out of 300 marks.
    
    For example:
      english = 56
      maths = 73
      science = 79
      Total Marks = 208
    
    Follow the steps given below to achieve the desired result:
    Step 1: Declare three variables english, maths and science respectively and store any numerical values in it.
    Step 2: Calculate the total marks by adding the values stored in the variables using the + operator. Store the result in the variable total.
    Step 3: Print the total score.

---
+++ 2: Display the total marks scored in 3 subjects.py
@@ -0,0 +1,12 @@
+# Write your code here
+
+# Step 1: Declare three variables 'english', 'maths' and 'science' respectively and store any numerical values in it
+english = 56
+maths = 74
+science = 80
+
+# Step 2: store their sum in 'total' variable
+total = english + maths + science
+
+# Step 3: Use the 'print()' function to display the total marks
+print("Total marks =", total)
