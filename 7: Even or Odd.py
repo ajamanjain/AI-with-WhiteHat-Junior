a0b49823953fa1f247a9074c4db597e8da3cbb65
Aman Jain <ajjaina.jain@gmail.com>
2021-10-19 21:22:13 +0530

    Even or Odd
    Accept a number from the user and check whether it is even or odd.
    
    For Example:
    
    Input: 24
    Ouput: Even number
    
    Follow the steps given below to achieve the desired result:
    
    Step 1: Ask the user to enter a number.
    
    Step 2: Convert the data type of user input to integer and then store it in a variable num.
    
    Step 3: Use if statement to determine whether the entered number is odd or even. For this, use modulo division (%). The result of a modulo division is the remainder of an integer division. If the number is completely divisible by 2, then print Even number else print Odd number.

---
+++ 7: Even or Odd.py
@@ -0,0 +1,10 @@
+# Write your code here
+
+# Step 1: Accept a number from the user. Convert it into an integer and then store it in a variable num
+num = int(input("Enter any integer number : "))
+
+# Step 2: Use if condition to check whether the number is even or odd using (%) operator
+if num%2==0:
+  print("Even number")
+else:
+  print("Odd number")
