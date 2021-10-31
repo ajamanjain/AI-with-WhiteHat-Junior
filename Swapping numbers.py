f8abae85101b4d72b180bc987d09e5286da9e218
Aman Jain <ajjaina.jain@gmail.com>
2021-10-31 22:14:11 +0530

    Swapping numbers
    Accept two numbers from the user. Store these numbers in two variables and swap the values of these variables.
    
    Example:
    num1: 10
    num2: 25
    Value of num1 after swapping:  25
    Value of num2 after swapping:  10
    
    Follow the steps given below to achieve the desired result:
    
    Step 1: Ask the user to enter two different numbers and store them in variables num1 and num2 respectively.
    Step 2: Declare one more variable temp which will be used to swap the values between num1 and num2. Assign the value of num1 variable to the temp variable.
    Step 3: Assign the value of num2 variable to num1 variable.
    Step 4: Assign the value of temp variable to num2 variable.
    Step 5: Print the values of num1 and num2 variables obtained after swapping.

---
+++ Swapping numbers.py
@@ -0,0 +1,14 @@
+# Write your code here
+# Step 1: Accept two numbers from the user and store them 'num1' and 'num2' variables
+num1 = input("num1: ")
+num2 = input("num2: ")
+# Swapping values of variables begins
+# Step 2: Declare a temporary variable 'temp' and assign the value of 'num1' to it
+temp = num1
+# Step 3: Assign value of 'num2' to 'num1'
+num1 = num2
+# Step 4: Assign value of 'temp' to 'num2'
+num2 = temp
+# Step 5: Print the values of 'num1' and 'num2' after swapping
+print("Value of num1 after swapping: ",num1)
+print("Value of num1 after swapping: ",num2)
